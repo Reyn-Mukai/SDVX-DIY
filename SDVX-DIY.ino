8b86d13edfc07ec0df4622b59511c85ad4a9e5fd
Reyn Mukai <reyn.mukai@gmail.com>
2018-04-30 22:13:41 -1000

    Fixed Array Key Events & Implemented Fixed NeoPixel Refresh Rate

--- SDVX-DIY.ino
+++ SDVX-DIY.ino
@@ -31,7 +31,8 @@
 #define NUM_LEDS_RING1 48
 #define MAX_STRIP_LEVEL 200
 #define MAX_RING_LEVEL 150
-#define DEGEN_FADE_INTERVAL 2
+#define DEGEN_FADE_INTERVAL 1000
+#define REFRESH_RATE 60
 
 /* Encoder */
 #define ENC_1_PIN_A 0
@@ -50,7 +51,7 @@ Encoder encRight(ENC_2_PIN_A, ENC_2_PIN_B);
 /* Button */
 unsigned int buttonPin[7] = {BTA_PIN, BTB_PIN, BTC_PIN, BTD_PIN, FxL_PIN, FxR_PIN, START_PIN};
 unsigned long keyTimer[7] = {0, 0, 0, 0, 0, 0, 0};
-bool state[7] = {false, false, false, false, false, false, false};
+bool buttonState[7] = {false, false, false, false, false, false, false};
 bool switchType[7] = {true, true, true, true, false, false, true};
 char asciiKey[7] = {0x64, 0x66, 0x6A, 0x6B, 0x6D, 0x63, 0x31};
 
@@ -60,6 +61,21 @@ unsigned int fadeLevelRight = 180;
 unsigned long fadeTimerLeft = 0;
 unsigned long fadeTimerRight = 0;
 unsigned int ledPin[7] = {BTA_LED, BTB_LED, BTC_LED, BTD_LED, FxL_LED, FxR_LED, START_LED};
+bool pixelState[7] = {false, false, false, false, false, false, false};
+int glowAddress[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+int bottomAddress [3] = {44, 45, 46};
+int topLeftAddress[3] = {12, 13, 14};
+int topRightAddress[3] = {18, 19, 20};
+int chevronLeft[5] = {27, 28, 41, 42, 43};
+int chevronRight [5] = {21, 34, 35, 36, 47};
+int startAddress[3] = {15, 16, 17};
+int btaAddress[4] = {25, 26, 29, 30};
+int btbAddress[4] = {24, 25, 30, 31};
+int btcAddress[4] = {23, 24, 31, 32};
+int btdAddress[4] = {22, 23, 32, 33};
+int fxlAddress[2] = {37, 38};
+int fxrAddress[2] = {39, 40};
+unsigned long refreshTimer = 0;
 
   /* Pixel Sectors
    * side lighting 0-11
@@ -100,110 +116,107 @@ void setup() {
   pinMode(BTD_LED, OUTPUT);
   pinMode(START_LED, OUTPUT);
   for(int i = 0; i < NUM_LEDS_STRIP; i++){
-    strip.setPixelColor(i, strip.Color(255, 50, 50));
+    //strip.setPixelColor(i, strip.Color(200, 50, 15));
+    strip.setPixelColor(i, strip.Color(100, 100, 100));
   }
   strip.show();
   for(int i = 0; i < NUM_LEDS_STRIP; i++){
     ring.setPixelColor(i, ring.Color(0, 0, 0));
   }
   ring.show();
+  Serial.begin(115200);
 }
 
 /* Main Loop */
 void loop() {
-  //checkAllKeyEvents();
-  checkSingleKeyEvent(0);
-  checkSingleKeyEvent(1);
-  checkSingleKeyEvent(2);
-  checkSingleKeyEvent(3);
-  checkSingleKeyEvent(4);
-  checkSingleKeyEvent(5);
-  checkSingleKeyEvent(6);
+  checkAllKeyEvents();
   encFuncLeft();
   encFuncRight();
   degenFadeLeft();
   degenFadeRight();
+  //reactiveLighting();
+  updateLighting();
 }
 
 void checkSingleKeyEvent(int i){
   if(switchType[i] == true){
-    if(digitalRead(buttonPin[i]) == LOW && state[i] == false){
+    if(digitalRead(buttonPin[i]) == LOW && buttonState[i] == false){
       Keyboard.press(asciiKey[i]);
       digitalWrite(ledPin[i], HIGH);
-      state[i] = true;
+      buttonState[i] = true;
       keyTimer[i] = millis();
     }
-    else if(digitalRead(buttonPin[i]) == HIGH && state[i] == true && millis() - keyTimer[i] > DEBOUNCE){
+    else if(digitalRead(buttonPin[i]) == HIGH && buttonState[i] == true && millis() - keyTimer[i] > DEBOUNCE){
       Keyboard.release(asciiKey[i]);
       digitalWrite(ledPin[i], LOW);
-      state[i] = false;
+      buttonState[i] = false;
     }
   }
   else{
-    if(digitalRead(buttonPin[i]) == HIGH && state[i] == false){
+    if(digitalRead(buttonPin[i]) == HIGH && buttonState[i] == false){
       Keyboard.press(asciiKey[i]);
       digitalWrite(ledPin[i], HIGH);
-      state[i] = true;
+      buttonState[i] = true;
       keyTimer[i] = millis();
     }
-    else if(digitalRead(buttonPin[i]) == LOW && state[i] == true && millis() - keyTimer[i] > DEBOUNCE){
+    else if(digitalRead(buttonPin[i]) == LOW && buttonState[i] == true && millis() - keyTimer[i] > DEBOUNCE){
       Keyboard.release(asciiKey[i]);
       digitalWrite(ledPin[i], LOW);
-      state[i] = false;
+      buttonState[i] = false;
     }
   }
 }
 
 void checkAllKeyEvents(){
-  for(int i = 0; i < sizeof(buttonPin); i++){
+  for(int i = 0; i < sizeof(buttonPin)/2; i++){
     if(switchType[i] == true){
-      if(digitalRead(buttonPin[i]) == LOW && state[i] == false){
+      if(digitalRead(buttonPin[i]) == LOW && buttonState[i] == false){
         Keyboard.press(asciiKey[i]);
         digitalWrite(ledPin[i], HIGH);
-        state[i] = true;
+        buttonState[i] = true;
         keyTimer[i] = millis();
       }
-      else if(digitalRead(buttonPin[i]) == HIGH && state[i] == true && millis() - keyTimer[i] > DEBOUNCE){
+      else if(digitalRead(buttonPin[i]) == HIGH && buttonState[i] == true && millis() - keyTimer[i] > DEBOUNCE){
         Keyboard.release(asciiKey[i]);
         digitalWrite(ledPin[i], LOW);
-        state[i] = false;
+        buttonState[i] = false;
       }
     }
     else{
-      if(digitalRead(buttonPin[i]) == HIGH && state[i] == false){
+      if(digitalRead(buttonPin[i]) == HIGH && buttonState[i] == false){
         Keyboard.press(asciiKey[i]);
         digitalWrite(ledPin[i], HIGH);
-        state[i] = true;
+        buttonState[i] = true;
         keyTimer[i] = millis();
       }
-      else if(digitalRead(buttonPin[i]) == LOW && state[i] == true && millis() - keyTimer[i] > DEBOUNCE){
+      else if(digitalRead(buttonPin[i]) == LOW && buttonState[i] == true && millis() - keyTimer[i] > DEBOUNCE){
         Keyboard.release(asciiKey[i]);
         digitalWrite(ledPin[i], LOW);
-        state[i] = false;
+        buttonState[i] = false;
       }
     }
   }
 }
 
 void degenFadeLeft(){
-  if(millis() - fadeTimerLeft >= DEGEN_FADE_INTERVAL && fadeLevelLeft > 0){
+  if(micros() - fadeTimerLeft >= DEGEN_FADE_INTERVAL && fadeLevelLeft > 0){
     for(int i = 24; i < 48; i++){
       ring.setPixelColor(i, ring.Color(0, 0, fadeLevelLeft));
     }
     fadeLevelLeft--;
-    fadeTimerLeft = millis();
+    fadeTimerLeft = micros();
   }
-  ring.show();
+  //ring.show();
 }
 void degenFadeRight(){
-  if(millis() - fadeTimerRight >= DEGEN_FADE_INTERVAL && fadeLevelRight > 0){
+  if(micros() - fadeTimerRight >= DEGEN_FADE_INTERVAL && fadeLevelRight > 0){
     for(int i = 0; i < 24; i++){
       ring.setPixelColor(i, ring.Color(fadeLevelRight, 0, 0));
     }
     fadeLevelRight--;
-    fadeTimerRight = millis();
+    fadeTimerRight = micros();
   }
-  ring.show();
+  //ring.show();
 }
 
 void encFuncLeft(){
@@ -232,5 +245,30 @@ void updateMousePositionLeft(){
 
 void updateMousePositionRight(){
   Mouse.move(0, encRight.read(), 0);
+}
+
+void reactiveLighting(){
+  if(buttonState[6] == true && pixelState[6] == false){
+    for(int i = 0; sizeof(startAddress)/2; i++){
+      strip.setPixelColor(startAddress[i], strip.Color(200, 50, 15));
+    }
+    pixelState[6] = true;
+  }
+  else if(buttonState[6] == false && pixelState[6] == true){
+    for(int i = 0; sizeof(startAddress)/2; i++){
+      strip.setPixelColor(startAddress[i], strip.Color(100, 100, 100));
+    }
+    pixelState[6] = false;
+  }
+  Serial.print(buttonState[6]);
+  Serial.println(pixelState[6]);
+  //strip.show();
+}
+
+void updateLighting(){
+  if((millis() - refreshTimer) > (1000 / REFRESH_RATE)){
+    refreshTimer = millis();
+    ring.show();
+  }
 }
 
